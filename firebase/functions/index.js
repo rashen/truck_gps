9e77e39b5a70f594dd6d37178ab32dbc046407e5
Rasmus Henriksson <rasmhenriksson@gmail.com>
2018-05-29 17:57:16 +0200

    Set up backend function to circumvent issues with POST/PUT

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,18 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// This is a workaround where data is moved from input struct. It is needed
+// as the SIM module can't do PUT requests.
+exports.redirectData = functions.database.ref('input/{pushId}').onCreate((snapshot, context) => {
+    const original = snapshot.val();
+    snapshot.ref.remove();
+    console.log('Moving ', context.params.pushId, original);
+    return snapshot.ref.parent.parent.child('Sigmatrucken').set((original), (error) => {
+        if (error) {
+            console.log('Data could not be written. I have not clue why.');
+        } else {
+            snapshot.ref.remove();
+            console.log('Original data removed successfully');
+        }
+    });
+});
