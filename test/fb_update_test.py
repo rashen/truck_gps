3e85e026a5c82f355596dadc071d2dda70bfb4ba
Rasmus Henriksson <rasmhenriksson@gmail.com>
2018-06-01 16:02:25 +0200

    Made arduino module upload real GPS coordinates

--- test/fb_update_test.py
+++ test/fb_update_test.py
@@ -1,16 +1,16 @@
 import requests
 import os.path
 import re
+import json
 
-position = {'lat': 57.706309,
+
+with open(os.path.dirname(__file__) + '/../secrets.h') as f:
+    url = re.match('#define BACKEND_URL \"(.*)\"', f.readline()).group(1)
+    api_key = re.match('#define API_KEY \"(.*)\"', f.readline()).group(1)
+    
+    data = {'key': api_key,
+            'lat': 57.706309,
             'lng': 11.940235}
 
-with open(os.path.dirname(__file__) + '/../private_url.h') as f:
-    with open(os.path.dirname(__file__) + '/../private_api_key.h') as g:
-        url = re.match('#define BACKEND_URL \"(.*)\"', f.readline()).group(1)
-        api_key = re.match('#define API_KEY \"(.*)\"', g.readline()).group(1)
-        
-        key = {'key': api_key}
-
-        r = requests.post(url, json={**key, **position})
-        print(r.status_code)
+    r = requests.post(url, data=json.dumps(data))
+    print(r.status_code)
--- truck_gps.ino
+++ truck_gps.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 #include "Adafruit_FONA.h"
-#include "private_url.h" // stores BACKEND_URL
+#include "secrets.h" /* stores BACKEND_URL and API_KEY */
 
 #define FONA_RX (2)
 #define FONA_TX (3)
@@ -8,6 +8,10 @@
 
 #define BAUD_FONA (4800)
 #define BAUD_USB (115200)
+
+#define PREC_NONE (0)
+#define PREC_GSM  (1)
+#define PREC_GPS  (2)
 
 char reply_buffer[255];
 
@@ -18,15 +22,15 @@ Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
 typedef struct
 {
-    // Should add some type of uncertainty here
     float lat;
     float lng;
-    float spd;
-    float hdg;
-    float alt;
+    float speed;
+    float heading;
+    float altitude;
+    uint8_t precision = PREC_NONE;
 } pos_t;
 
-pos_t pos = {0};
+static pos_t gs_pos;
 
 void setup()
 {
@@ -40,10 +44,6 @@ void setup()
         while(1); // Hang if no FONA
     }
 
-    // Save for later?
-    char imei[16] = {0};
-    fona.getIMEI(imei);
-
     // Enable GPRS
     fona.setGPRSNetworkSettings(F("services.telenor.se"));
     while (!fona.enableGPRS(true))
@@ -53,59 +53,33 @@ void setup()
         delay(3000);
     }
     fona.setHTTPSRedirect(true);
-
-    // Enable GPS
-    while (!fona.enableGPS(true))
-    {
-        Serial.println(F("Failed to turn on GPS, retry in 3s"));
-        fona.enableGPS(false);
-        delay(3000);
-    }
 }
 
-void loop()
+static bool m_post_coordinates(void)
 {
+    static char api_key[] = API_KEY;
+    static char url[] = BACKEND_URL;
 
-    boolean gps_success = fona.getGPS(&pos.lat, &pos.lng, NULL, NULL, NULL);
-
-    // False if GPRS location hasn't been fetched first
-    if (gps_success == false)
-    {
-        Serial.println(F("Failed to fetch GPS coordinates"));
-
-        int gps_status = fona.GPSstatus();
-        Serial.print(F("GPS status: "));
-        Serial.print(gps_status);
-
-        fona.getGSMLoc(&pos.lat, &pos.lng);
-    }
+    static char lat_c[16] = {0};
+    static char lng_c[16] = {0};
+    static char data[100] = {0};
 
     uint16_t status_code;
     uint16_t len;
-    char url[] = BACKEND_URL;
 
-    if (!fona.HTTP_GET_start(url, &status_code, &len))
-    {
-        Serial.println(F("Get failed"));
-    }
-    // Read response buffer
-    while (len > 0)
-    {
-        while (fona.available())
-        {
-            char c = fona.read();
-            Serial.write(c);
-            len--;
-        }
-    }
-    fona.HTTP_GET_end();
+    dtostrf(gs_pos.lat, 4, 10, lat_c);
+    dtostrf(gs_pos.lng, 4, 10, lng_c);
 
-    char data[] = "{\"lat\":12.106168, \"lng\": 12.106168}";
+    snprintf(data, sizeof(data), "{\"key\": \"%s\", \"lat\": %s, \"lng\": %s, \"prc\": %d}", api_key, lat_c, lng_c, gs_pos.precision);
+
+    Serial.println(data);
 
     if (!fona.HTTP_POST_start(url, F("text/plain"), (uint8_t *) data, strlen(data), &status_code, &len))
     {
         Serial.println(F("Post failed"));
+        return false;
     }
+
     // Read response buffer
     while (len > 0)
     {
@@ -117,12 +91,37 @@ void loop()
         }
     }
     fona.HTTP_POST_end();
+    return true;
+}
+
+void loop()
+{
+    int gps_status = fona.GPSstatus();
+    Serial.print(F("GPS status: "));
+    Serial.print(gps_status);
+
+    if (fona.getGPS(&gs_pos.lat, &gs_pos.lng, NULL, NULL, NULL))
+    {
+        Serial.println("Fetched GPS location successfully");
+        gs_pos.precision = PREC_GPS;
+    }
+    else
+    {
+        Serial.println(F("Failed to fetch GPS coordinates, fetching GSM location instead"));
+        fona.getGSMLoc(&gs_pos.lat, &gs_pos.lng);
+        gs_pos.precision = PREC_GSM;
+    }
+
+    if (!m_post_coordinates())
+    {
+        Serial.println(F("Failed to post coordinates to backend"));
+    }
 
     Serial.print(F("Location is "));
-    Serial.print(pos.lat, 12);
+    Serial.print(gs_pos.lat, 12);
     Serial.print(F(" , "));
-    Serial.print(pos.lng, 12);
+    Serial.print(gs_pos.lng, 12);
     Serial.print(F("\n"));
 
-    delay(5000);
+    delay(30000);
 }
